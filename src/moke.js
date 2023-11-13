bb8f6e79bdc3addd5f2608fb3175b17eec4257b7
Denis Kalkopf <deniskalkopf@gmail.com>
2023-11-13 13:21:09 +0500

    add project

--- src/moke.js
+++ src/moke.js
@@ -1,14 +1,27 @@
 export const projects = [
   {
+    id: 10,
+    type: 'pet-проекты',
+    name: 'Spotify-Clone',
+    image: 'images/spotify-clone.png',
+    title: 'Full stack проект, музыкальная платформа.',
+    description: 'Nest JS, Next JS, TypeScript, Mongo DB',
+    opinion:
+      'Реализована загрузка, прослушивание и удаление музыкальных треков, загрузка изображений для обложки трека. Есть возможность оставлять комментарии. Так же реализована работа музыкального плеера.',
+    repolinkFront: 'https://github.com/DenisJet/spotify-clone-client',
+    repolinkBack: 'https://github.com/DenisJet/spotify-clone-server',
+  },
+  {
     id: 9,
     type: 'pet-проекты',
     name: 'BLOG ME',
     image: 'images/blog-me.png',
     title: 'Блог, с возможностью написания статей.',
     description:
-      'Пэт проект. MongoDB, Express, React, NodeJS, Material Ui, Redux Toolkit, Axios, JWT, BCrypt, Multer',
+      'MongoDB, Express, React, NodeJS, Material Ui, Redux Toolkit, Axios, JWT, BCrypt, Multer',
     opinion:
       'Первый мой full stack проект, за время написания которого научился создавать свой бэкенд. В проекте реализована регистрация и авторизация пользователей. Чтение, создание, удаление, изменение статей (CRUD). Загрузка изображений на сервер. Возможность оставлять комментарии и т.д.',
+    link: 'https://blogmern.netlify.app/',
     repolinkFront: 'https://github.com/DenisJet/blog-mern-front',
     repolinkBack: 'https://github.com/DenisJet/blog-mern-beck',
   },
@@ -18,8 +31,7 @@ export const projects = [
     name: 'Github Seasch',
     image: 'images/githubsearch.png',
     title: 'Поиск репозиториев на Гитхабе по имени пользователя.',
-    description:
-      'Пэт проект. TypeScript, React, Redux, Redux Toolkit, RTKQuery, Tailwind CSS. Axios.',
+    description: 'TypeScript, React, Redux, Redux Toolkit, RTKQuery, Tailwind CSS. Axios.',
     opinion:
       'Несложный проект, по поиску репозиториев на Гитхабе. За время реализации проекта познакомился с RTKQuery, Tailwind CSS. Это первый проект на TypeScript',
     link: 'https://denisjet-githubsearch.netlify.app/',
@@ -45,7 +57,7 @@ export const projects = [
     image: 'images/cleanhouse.png',
     title: 'Сайт службы по уничтожению насекомых.',
     description:
-      'Коммерческий проект. Семантическая, валидная, адаптивная, резиновая, кроссбраузерная вёрстка. БЭМ, Sass, Gulp. Оптимизация изображений.',
+      'Семантическая, валидная, адаптивная, резиновая, кроссбраузерная вёрстка. БЭМ, Sass, Gulp. Оптимизация изображений.',
     opinion: 'Сайт полностью мной придуман, разработан и выложен на хостинг.',
     link: 'https://cleanhouse-ses.ru',
     repolinkFront: 'https://github.com/DenisJet/CleanHouse',
@@ -56,7 +68,7 @@ export const projects = [
     name: 'Большое путешествие',
     image: 'images/big-trip.png',
     title: 'Cовременный сервис для путешественников.',
-    description: 'Учебный проект. SPA на чистом JavaScript. ООП. MVP. REST Api',
+    description: 'SPA на чистом JavaScript. ООП. MVP. REST Api',
     opinion:
       'Сервис помогает детально спланировать маршрут поездки, рассчитать стоимость путешествия и получить информацию о достопримечательностях. За время реализации проекта познакомился с ООП. В проекте используется классовый подход. Научился применять архитектурный паттерн MVP. Разобрался с REST Api. Так же используются паттерны Адаптер и Observer (Наблюдатель). Познакомился с работой сервис-воркеров.',
     link: 'https://denisjet-big-trip.netlify.app/',
@@ -68,8 +80,7 @@ export const projects = [
     name: 'Фермерские продукты',
     image: 'images/fermers-products.png',
     title: 'Пример интернет магазина фермерских продуктов.',
-    description:
-      'Учебный проект. Использование библиотеки React. Styled-components, react hooks, react-router.',
+    description: 'Использование библиотеки React. Styled-components, react hooks, react-router.',
     opinion:
       'За время реализации проекта научился создавать SPA, используя функциональные компоненты, стилизовать компоненты при помощи styled-components и управлять состоянием и интерактивностью компонентов при помощи основных react хуков (useState, useEffect). А так же разобрался как  устроена маршрутизация в SPA и что такое react-router. Использованна сторонняя библиотека swiper.js.',
     link: 'https://denisjet-fermers-products.netlify.app/',
@@ -82,7 +93,7 @@ export const projects = [
     image: 'images/kekstagram.png',
     title: 'Кекстаграм — сервис просмотра изображений.',
     description:
-      'Учебный проект. Пользователям предоставлена возможность загружать свои фотографии или просматривать фотографии, загруженные ранее другими пользователями.',
+      'Пользователям предоставлена возможность загружать свои фотографии или просматривать фотографии, загруженные ранее другими пользователями.',
     opinion:
       'За время реализации проекта работал с объектами и массивами, с событиями и DOM деревом. А так же присутствует работа с сетью. Использованна сторонняя библиотека noUiSlider.',
     link: 'https://denis-jet-kekstagram.netlify.app/',
@@ -95,7 +106,7 @@ export const projects = [
     image: 'images/keksobooking.png',
     title: 'Cервис по поиску и размещения объявлений о сдаче в аренду недвижимости в центре Токио.',
     description:
-      'Учебный проект. Пользователям предоставляется возможность размещать объявления о своей недвижимости или просматривать уже размещённые объявления.',
+      'Пользователям предоставляется возможность размещать объявления о своей недвижимости или просматривать уже размещённые объявления.',
     opinion:
       'За время реализации проекта научился работать c объектами и массивами, с DOM деревом и событиями в JavaScript. Научился работе с сетью: синхронные и асинхронные операции, JSON, промисы, Fetch API. В проекте реализована валидация формы с помощью Javascript.',
     link: 'https://denisjet-keksobooking.netlify.app/',
@@ -108,7 +119,7 @@ export const projects = [
     image: 'images/drink2go.png',
     title: 'Главная страница интернет магазина кофейных напитков.',
     description:
-      'Учебный проект. Семантическая, валидная, адаптивная, резиновая, кроссбраузерная (Chrome, Firefox, Safari), PixelPerfect вёрстка. БЭМ, Sass, Gulp. Ретинизация графики. Адаптивность и оптимизация изображений. Использование сторонних библиотек Leaflet и noUiSlider.',
+      'Семантическая, валидная, адаптивная, резиновая, кроссбраузерная (Chrome, Firefox, Safari), PixelPerfect вёрстка. БЭМ, Sass, Gulp. Ретинизация графики. Адаптивность и оптимизация изображений. Использование сторонних библиотек Leaflet и noUiSlider.',
     opinion:
       'Итоговый учебный проект курса Фронтенд-разработчик от HTML Academy, за время реализации которого я научился базовым стандартам верстки. В проекте так же присутствует не много JavaScript, это мобильное меню сайта и слайдер в промо-блоке.',
     link: 'https://denisjet-drink2go.netlify.app/',
