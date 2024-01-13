78753980468c6ad75c9835f4e2665d5ee83463aa
Denis Kalkopf <deniskalkopf@gmail.com>
2024-01-13 17:15:26 +0500

    fix

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { StyledFooter } from './styled';
 
 export default function Footer() {
-  return <StyledFooter>Copyright 2023</StyledFooter>;
+  return <StyledFooter>Copyright 2024</StyledFooter>;
 }
